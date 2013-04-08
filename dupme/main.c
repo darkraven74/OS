b8e071b8df9276ab106c721faab0f5fed9eb6f7c
Baev Vladimir <darkraven8@gmail.com>
2013-04-09 00:59:08 +0400

    string has_endl added

--- dupme/main.c
+++ dupme/main.c
@@ -14,20 +14,44 @@ int get_k(char* s)
     return ans;
 }
 
+int has_endl(char* s, int last)
+{
+    int cur = 0;
+    while ((s[cur] != 0) && (cur < last))
+    {
+        if (s[cur] == '\n')
+        {
+            return cur;
+        }
+        cur++;
+    }
+    return -1;
+}
+
 int main(int argc, char* argv[])
 {
-    int k = get_k(argv[1]);
+    int k = get_k(argv[1]) + 1;
     char* buf = malloc(k);
-    int len = 0;
+    int len = 0, count, eof = 0;
+
 
     while (1)
     {
-        read(0, buf, k);
-        break;
+        count = read(0, buf + len, k - len);
+        if (count == 0)
+        {
+            eof = 1;
+        }
+
+
+
+        if (k == len)
+        {
+
+        }
     }
 
-    write(1, buf, k);
-    printf("%d\n", k);
+
 
 	return 0;
 }
