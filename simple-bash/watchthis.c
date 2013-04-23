4f5775e1d1de1b905cd2b8484a721e8126f48e5f
Vladimir Baev <darkraven8@gmail.com>
2013-04-23 17:59:31 +0400

    input args parsing added

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -1,5 +1,32 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main()
+
+char* file1 = "temp1";
+char* file2 = "temp2";
+
+int main(int argc, char* argv[])
 {
+    int update = atoi(argv[1]);
+    char** args = malloc(argc - 1);
+    args[argc - 2] = NULL;
+    int i = 0;
+    for (i = 2; i < argc; i++)
+    {
+        args[i - 2] = argv[i];
+    }
+    while (1)
+    {
+        int pid = fork();
+        if (pid == 0)
+        {
+            //child
+            execlp("diff", "diff", file1, file2, NULL);
+        }
+        waitpid(pid, NULL, 0);
+
+        sleep(update);
+    }
     return 0;
 }
