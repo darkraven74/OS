3c646f23f94c17f54f90cebf61a4243f28cf1ba9
Vladimir Baev <darkraven8@gmail.com>
2013-04-23 18:20:26 +0400

    run func added

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -3,30 +3,40 @@
 #include <stdio.h>
 
 
-char* file1 = "temp1";
-char* file2 = "temp2";
+char* files[2] = {"temp1", "temp2"};
+
+void run(char** args, int file_id)
+{
+
+
+
+}
+
 
 int main(int argc, char* argv[])
 {
     int update = atoi(argv[1]);
     char** args = malloc(argc - 1);
     args[argc - 2] = NULL;
-    int i = 0;
+    int i = 0, file_id = 0;
     for (i = 2; i < argc; i++)
     {
         args[i - 2] = argv[i];
     }
+    run(args, 1); 
     while (1)
     {
+        run(args, file_id);
         int pid = fork();
         if (pid == 0)
         {
             //child
-            execlp("diff", "diff", file1, file2, NULL);
+            execlp("diff", "diff", files[0], files[1], NULL);
         }
         waitpid(pid, NULL, 0);
-
+        file_id = (file_id++) % 2;
         sleep(update);
     }
+    free(args);
     return 0;
 }
