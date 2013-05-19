4c6a63aedefdf02820d81533335e880652945606
Vladimir Baev <darkraven8@gmail.com>
2013-05-19 19:33:36 +0400

    tab fix

--- dupme/main.c
+++ dupme/main.c
@@ -16,52 +16,52 @@ int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-	    return 1;
+        return 1;
     }
     int k = atoi(argv[1]) + 1;
     if (k < 2)
     {
-	    return 2;
+        return 2;
     }
     char* buf = malloc(k);
     int len = 0, count, eof = 0, ignore = 0;
 
     while (!eof)
     {
-	    count = read(0, buf + len, k - len);
-	    if (count < 1)
-	    {
-		    eof = 1;		
-		    if (len > 0)
-		    {
-			    buf[len] = '\n';
-			    len++;
-		    }
-	    }
-	    len += count;
-	
-	    int cur = 0, start = 0;
-	    while (cur < len)
-	    {
-		    if (buf[cur] == '\n')
-		    {
-			    if (!ignore)
-			    {
-				    write_ans(buf, start, cur);
-			    }
-			    start = cur + 1;
-			    ignore = 0;
-		    }
-		    cur++;
-	    }
-	
-	    len -= start;
-	    memmove(buf, buf + start, len);
-	    if (len == k)
-	    {
-		    len = 0;
-		    ignore = 1;
-	    }	
+        count = read(0, buf + len, k - len);
+        if (count < 1)
+        {
+            eof = 1;		
+            if (len > 0)
+            {
+                buf[len] = '\n';
+                len++;
+            }
+        }
+        len += count;
+
+        int cur = 0, start = 0;
+        while (cur < len)
+        {
+            if (buf[cur] == '\n')
+            {
+                if (!ignore)
+                {
+                    write_ans(buf, start, cur);
+                }
+                start = cur + 1;
+                ignore = 0;
+            }
+            cur++;
+        }
+
+        len -= start;
+        memmove(buf, buf + start, len);
+        if (len == k)
+        {
+            len = 0;
+            ignore = 1;
+        }	
     }
 
     free(buf);
