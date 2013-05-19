1e87b78715adc67033dc0562a8736307cb0706e2
Vladimir Baev <darkraven8@gmail.com>
2013-05-20 01:14:20 +0400

    global fix

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -2,74 +2,83 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-
-char* files[2] = {"temp1", "temp2"};
-char* buf;
-int buf_size = 5000;
-
-void run(char** args, int file_id)
-{
-    int fds[2];
-    int k, len = 0;
-    pipe(fds);
-    int pid = fork();
-    if (pid == 0)
-    {
-        //child
-        dup2(fds[1], 1);
-        close(fds[0]);
-        close(fds[1]);
-        execvp(args[0], args);
-        exit(0);
-    }
-
-    close(fds[1]);
-    waitpid(pid, NULL, 0);
-    while (1)
-    {
-        k = read(fds[0], buf + len, buf_size - len);
-        if ((buf_size - len) <= 0)
-        {
-            break;
-        }
-        if (k == 0)
-        {
-            break;
-        }
-        len += k;
-    }
-    int f1 = open(files[file_id], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    write(f1, buf, len);
-    write(1, buf, len); 
-}
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
-    int update = atoi(argv[1]);
-    char** args = malloc(argc - 1);
-    args[argc - 2] = NULL;
-    int i = 0, file_id = 0;
-    for (i = 2; i < argc; i++)
+    if (argc < 3)
     {
-        args[i - 2] = argv[i];
+        return 1;
     }
-    buf = malloc(buf_size);
-    run(args, 1); 
-    while (1)
+    int update = atoi(argv[1]);
+    if (update < 1)
     {
-        run(args, file_id);
+        return 2;    
+    }
+    
+    const char* f_old = "file_old";
+    const char* f_new = "file_new";
+ 
+    char** args = malloc(sizeof(char*) * (argc - 1));
+    memcpy(args, argv + 2, sizeof(char*) * (argc - 2));
+    args[argc - 2] = NULL;
+    
+    int buf_size = 5000;
+    char* buf = malloc(buf_size);
+    char* old_buf = malloc(buf_size);
+    int old_len = 0;
+        
+    while (1)
+    {  
+        int fds[2];
+        int k, len = 0;
+        pipe(fds);
         int pid = fork();
         if (pid == 0)
         {
             //child
-            execlp("diff", "diff", files[0], files[1], NULL);
+            dup2(fds[1], 1);
+            close(fds[0]);
+            close(fds[1]);
+            execvp(args[0], args);
+            exit(0);
+        }
+
+        close(fds[1]);
+        waitpid(pid, NULL, 0);
+        do
+        {
+            k = read(fds[0], buf + len, buf_size - len);
+            len += k;
+        } while (k > 0 && buf_size - len != 0);
+        close(fds[0]);
+        
+        int fd_old = open(f_old, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        int fd_new = open(f_new, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        write(fd_old, old_buf, old_len);
+        write(fd_new, buf, len);
+        write(1, buf, len);  
+        close(fd_old);
+        close(fd_new);       
+        
+        memcpy(old_buf, buf, len);
+        old_len = len;
+        
+        pid = fork();
+        if (pid == 0)
+        {
+            //child
+            execlp("diff", "diff", "-u", f_old, f_new, NULL);
+            exit(0);
         }
         waitpid(pid, NULL, 0);
-        file_id = (file_id++) % 2;
         sleep(update);
     }
     free(args);
     free(buf);
+    free(old_buf);
     return 0;
 }
